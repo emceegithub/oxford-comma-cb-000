3e5c9e9e363d1d534317784b348f0a38b37ba90d
Mark Cruz <markchriscruz@yahoo.com>
2018-03-06 07:07:44 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,13 @@ def oxford_comma(array)
     return array[0]
   elsif array.length == 2
     return array.join(" and ")
-  elsif array.length == 3
-    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+  elsif array.length >= 3
+    string = ""
+    lastElement = array.last
+    array.pop
+    string = array.join(", ")
+    string = "#{string}, and #{lastElement}"
+    return string
   else
     #do nothing
   end
