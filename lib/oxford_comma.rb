1ec54578efc03e836b2660395579676802352cac
Mark Cruz <markchriscruz@yahoo.com>
2018-03-06 06:37:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,6 +3,9 @@ def oxford_comma(array)
     return array[0]
   elsif array.length == 2
     return array.join(" and ")
+  elsif array.length == 3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}
   else
+    #do nothing
   end
 end
